cf18a394c92bdc7ba6c3c20c129e0f578665db01
lawrenceyim <lawrence.yim.010@gmail.com>
2023-10-24 12:27:08 -0400

    Rewrote quick sort using Generics and Comparable

--- HomeTask_1_SortingAlgorithm/src/main/java/solvdlaba/hometask/Main.java
+++ HomeTask_1_SortingAlgorithm/src/main/java/solvdlaba/hometask/Main.java
@@ -4,43 +4,38 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array1 = new int[] {5, 400, 33, -2, 100, 0};
+        Integer[] array1 = {5, 400, 33, -2, 100, 0};
         quickSort(array1);
         System.out.println(Arrays.toString(array1));
 
-        char[] array2 = new char[] {'d', 'z', 'b', 'y'};
+        Character[] array2 = {'d', 'z', 'b', 'y'};
         quickSort(array2);
         System.out.println(Arrays.toString(array2));
     }
 
-    // Method overloading to reduce arguments needed to call the quickSort function
-    public static void quickSort(int[] array) {
+    public static <T extends Comparable<T>> void quickSort(T[] array) {
         quickSort(array, 0, array.length - 1);
     }
 
-    public static void quickSort(char[] characters) {
-        quickSort(characters, 0, characters.length - 1);
-    }
-
-    public static void quickSort(int[] array, int minIndex, int maxIndex) {
+    public static <T extends Comparable<T>> void quickSort(T[] array, int minIndex, int maxIndex) {
         // Base case for recursion
         if (minIndex >= maxIndex) return;
 
-        int pivot = array[maxIndex];
+        T pivot = array[maxIndex];
         int minPtr = minIndex;
         int maxPtr = maxIndex - 1;
 
         // Rearrange array elements based on the pivot value
         while (minPtr < maxPtr) {
-            while (array[minPtr] <= pivot && minPtr < maxPtr) {
+            while (array[minPtr].compareTo(pivot) <= 0 && minPtr < maxPtr) {
                 minPtr++;
             }
-            while (array[maxPtr] >= pivot && maxPtr > minPtr) {
+            while (array[maxPtr].compareTo(pivot) >= 0 && maxPtr > minPtr) {
                 maxPtr--;
             }
             swap(array, minPtr, maxPtr);
         }
-        if (array[minPtr] > array[maxIndex]) {
+        if (array[minPtr].compareTo(array[maxIndex]) > 0) {
             swap(array, minPtr, maxIndex);
         } else {
             minPtr = maxIndex;
@@ -51,41 +46,9 @@ public class Main {
         quickSort(array, minPtr + 1, maxIndex);
     }
 
-    public static void quickSort(char[] array, int minIndex, int maxIndex) {
-        if (minIndex >= maxIndex) return;
-
-        int pivot = array[maxIndex];
-        int minPtr = minIndex;
-        int maxPtr = maxIndex - 1;
-
-        while (minPtr < maxPtr) {
-            while (array[minPtr] <= pivot && minPtr < maxPtr) {
-                minPtr++;
-            }
-            while (array[maxPtr] >= pivot && maxPtr > minPtr) {
-                maxPtr--;
-            }
-            swap(array, minPtr, maxPtr);
-        }
-        if (array[minPtr] > array[maxIndex]) {
-            swap(array, minPtr, maxIndex);
-        } else {
-            minPtr = maxIndex;
-        }
-
-        quickSort(array, minIndex, minPtr - 1);
-        quickSort(array, minPtr + 1, maxIndex);
-    }
-    
     // Helper function for quickSort
-    public static void swap(int[] array, int index1, int index2) {
-        int temp = array[index1];
-        array[index1] = array[index2];
-        array[index2] = temp;
-    }
-
-    public static void swap(char[] array, int index1, int index2) {
-        char temp = array[index1];
+    public static <T> void swap(T[] array, int index1, int index2) {
+        T temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
     }
