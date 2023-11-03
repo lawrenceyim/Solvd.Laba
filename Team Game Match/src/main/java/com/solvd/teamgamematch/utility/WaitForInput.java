cb5acda7a1da4998e7c7612ab33e69d5d746f279
lawrenceyim <lawrence.yim.010@gmail.com>
2023-11-03 09:53:43 -0400

    Added TODO for fixing Scanner bug

--- Team Game Match/src/main/java/com/solvd/teamgamematch/utility/WaitForInput.java
+++ Team Game Match/src/main/java/com/solvd/teamgamematch/utility/WaitForInput.java
@@ -12,6 +12,11 @@ import java.util.Scanner;
 public class WaitForInput {
     public static void waitForAnyUserInput() {
         System.out.println("Press enter to continue...");
+        /*
+        TODO: Scanner should be closed after it's used, but it shares System.in
+         with the scanner in the Main class. Closing this creates an error.
+         Figure out a solution for this problem
+         */
         Scanner input = new Scanner(System.in);
         input.nextLine();
     }
