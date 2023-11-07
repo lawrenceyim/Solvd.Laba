74b57715443e0ae3d665d96b868268d4bb110ee8
lawrenceyim <lawrence.yim.010@gmail.com>
2023-11-07 11:25:54 -0500

    Moved champion generation to utility

--- src/main/java/com/solvd/teamgamematch/game/ChampionManager.java
+++ src/main/java/com/solvd/teamgamematch/game/ChampionManager.java
@@ -1,6 +1,6 @@
 package com.solvd.teamgamematch.game;
 
-import com.solvd.teamgamematch.game.abilities.*;
+import com.solvd.teamgamematch.utility.GenerateChampions;
 
 import java.util.ArrayList;
 
@@ -20,8 +20,7 @@ public class ChampionManager {
     }
 
     private ChampionManager() {
-        champions = new ArrayList<>();
-        createChampions();
+        champions = GenerateChampions.generateChampions();
     }
 
     public static ChampionManager getInstance() {
@@ -31,33 +30,12 @@ public class ChampionManager {
         return instance;
     }
 
-    public void createChampions () {
-        champions.add(new Champion("Aatrox", "Top", 580, 100,
-                60, 0, 38, 32));
-        champions.add(new Champion("Zoe", "Mid", 560, 495,
-                58, 0, 20, 30));
-        champions.add(new Champion("Jinx", "ADC", 517, 245,
-                63, 0, 22, 30));
-        champions.add(new Champion("Thresh", "Support", 560, 273,
-                55, 0, 16, 30));
-        champions.add(new Champion("Lee Sin", "Jungle", 570, 200,
-                65, 0, 36, 32));
-        champions.add(new Champion("Ahri", "Mid", 526, 418,
-                53, 0, 20, 30));
-        champions.add(new Champion("Caitlyn", "ADC", 481, 313,
-                63, 0, 22, 30));
-        champions.add(new Champion("Lulu", "Support", 535, 350,
-                47, 0, 19, 30));
-        champions.add(new Champion("Jarvan IV", "Jungle", 571, 302,
-                64, 0, 33, 32));
-        champions.add(new Champion("Darius", "Top", 582, 263,
-                64, 0, 39, 32));
-        champions.add(new Champion("Orianna", "Mid", 530, 418,
-                48, 0, 17, 30));
-        champions.add(new Champion("Ezreal", "ADC", 500, 375,
-                56, 0, 22, 30));
-        champions.add(new Champion("Janna", "Support", 500, 350,
-                45, 0, 16, 30));
+    public ArrayList<Champion> getChampions() {
+        return champions;
+    }
+
+    public void setChampions(ArrayList<Champion> champions) {
+        this.champions = champions;
     }
 
     public ArrayList<String> getChampionNames() {
---
+++ src/main/java/com/solvd/teamgamematch/utility/GenerateChampions.java
@@ -0,0 +1,39 @@
+package com.solvd.teamgamematch.utility;
+
+import com.solvd.teamgamematch.game.Champion;
+
+import java.util.ArrayList;
+
+public class GenerateChampions {
+
+    public static ArrayList<Champion> generateChampions () {
+        ArrayList<Champion> champions = new ArrayList<>();
+        champions.add(new Champion("Aatrox", "Top", 580, 100,
+                60, 0, 38, 32));
+        champions.add(new Champion("Zoe", "Mid", 560, 495,
+                58, 0, 20, 30));
+        champions.add(new Champion("Jinx", "ADC", 517, 245,
+                63, 0, 22, 30));
+        champions.add(new Champion("Thresh", "Support", 560, 273,
+                55, 0, 16, 30));
+        champions.add(new Champion("Lee Sin", "Jungle", 570, 200,
+                65, 0, 36, 32));
+        champions.add(new Champion("Ahri", "Mid", 526, 418,
+                53, 0, 20, 30));
+        champions.add(new Champion("Caitlyn", "ADC", 481, 313,
+                63, 0, 22, 30));
+        champions.add(new Champion("Lulu", "Support", 535, 350,
+                47, 0, 19, 30));
+        champions.add(new Champion("Jarvan IV", "Jungle", 571, 302,
+                64, 0, 33, 32));
+        champions.add(new Champion("Darius", "Top", 582, 263,
+                64, 0, 39, 32));
+        champions.add(new Champion("Orianna", "Mid", 530, 418,
+                48, 0, 17, 30));
+        champions.add(new Champion("Ezreal", "ADC", 500, 375,
+                56, 0, 22, 30));
+        champions.add(new Champion("Janna", "Support", 500, 350,
+                45, 0, 16, 30));
+        return champions;
+    }
+}
